bcd84d008655f6df4cb94d3e48b9820437585239
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-25 00:52:56 -0700

    working version of spotify-this-song/should cleanup code

--- liri.js
+++ liri.js
@@ -3,7 +3,8 @@ var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var moment = require('moment')
-var spotify = new Spotify(keys.spotify);
+
+
 
 if (dot.error) {
     throw result.error
@@ -16,11 +17,10 @@ var cmd = cmds[2];
 
 // 
 cmds.splice(0,3); 
-var urlarg = cmds.join("+");
-
+var urlarg = cmds.join(" ");
 switch(cmd){
    case("concert-this"): concertThis(urlarg); break; // call concert function 
-   case("spotify-this-song"): break; // call a spotify function
+   case("spotify-this-song"): spotifyThisSong(urlarg); break; // call a spotify function
    case("movie-this"): break; // call the movie function
    case("do-what-it-says"): break; // does another function
    case(""): console.log(" no command given")
@@ -61,14 +61,30 @@ function concertThis(artist){
       
 }
 
-function spotifyThis(song){
+function spotifyThisSong(song){
+    var spotify = new Spotify(keys.spotify);
+    
+    // get just the first result (assuming in order of how close search matches results)
 
+    spotify
+        .search({ type: 'track', query: song ,limit: 1})
+        .then(function(response) {
+           
+            var item = response.tracks.items[0];
+            var timeFrmt = moment(item.album.release_date,"YYYY-MM-DD").format("MM/DD/YYYY")
+            console.log(item.name)
+            console.log(timeFrmt);
+            console.log(item.artists[0].name)
+            if(item.preview_url){console.log(item.preview_url)}
+            else{console.log("  ~~> \""+item.name+"\" can not be previewed")}
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
 function movieThis(movie){
 
 }
 function followDirections(direction){
-    if(direction.split(" ").length>1){
-
-    }
+ 
 }
